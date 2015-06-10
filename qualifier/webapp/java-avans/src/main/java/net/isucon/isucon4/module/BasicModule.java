8a918b97962120b7881d60c53b347491f70b94ed
Manabu Matsuzaki <matsumana@gmail.com>
2015-06-11 02:04:50 +0900

    fix bug

--- qualifier/webapp/java-avans/src/main/java/net/isucon/isucon4/module/BasicModule.java
+++ qualifier/webapp/java-avans/src/main/java/net/isucon/isucon4/module/BasicModule.java
@@ -1,35 +1,30 @@
 package net.isucon.isucon4.module;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Scopes;
-import freemarker.template.Configuration;
 import net.isucon.isucon4.config.Config;
 import net.isucon.isucon4.provider.ConfigProvider;
-import net.isucon.isucon4.provider.web.FreemarkerConfigurationProvider;
+
+import com.google.inject.AbstractModule;
 
 public class BasicModule extends AbstractModule {
-    private final Config config;
+	private final Config config;
 
-    public BasicModule() {
-        this.config = null;
-    }
+	public BasicModule() {
+		this.config = null;
+	}
 
-    public BasicModule(final Config config) {
-        this.config = config;
-    }
+	public BasicModule(final Config config) {
+		this.config = config;
+	}
 
-    @Override
-    protected void configure() {
-        if (config != null) {
-            bind(Config.class)
-                    .toInstance(config);
-        } else {
-            bind(Config.class)
-                    .toProvider(ConfigProvider.class)
-                    .asEagerSingleton();
-        }
-        bind(Configuration.class)
-                .toProvider(FreemarkerConfigurationProvider.class)
-                .in(Scopes.SINGLETON);
-    }
+	@Override
+	protected void configure() {
+		if (config != null) {
+			bind(Config.class)
+				.toInstance(config);
+		} else {
+			bind(Config.class)
+				.toProvider(ConfigProvider.class)
+				.asEagerSingleton();
+		}
+	}
 }
