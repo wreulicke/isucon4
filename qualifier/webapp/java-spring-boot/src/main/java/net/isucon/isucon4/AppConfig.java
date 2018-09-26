d3c3f7188cf58fd1fffdc52e53a7d8a248745049
wreulicke <saito.masaya@classmethod.jp>
2018-09-27 02:52:48 +0900

    update spring boot

--- qualifier/webapp/java-spring-boot/src/main/java/net/isucon/isucon4/AppConfig.java
+++ qualifier/webapp/java-spring-boot/src/main/java/net/isucon/isucon4/AppConfig.java
@@ -27,7 +27,7 @@ package net.isucon.isucon4;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.embedded.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
